693eb36dec1bc660b25c85cd85bb11a7b738696f
suparjotamin <61368465+suparjotamin@users.noreply.github.com>
2020-09-28 11:27:45 +0700

    Add files via upload

--- Stockie/report.py
+++ Stockie/report.py
@@ -11,8 +11,10 @@ import os
 import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
+import Stockie
 
-local_path = '/'.join(yf.__file__.split('/')[:-2]) + '/Stockie/'
+#local_path = '/'.join(yf.__file__.split('/')[:-2]) + '/Stockie/'
+local_path = Stockie.__file__[:-11]
 
 def create_label(df, timegap):
     return np.array(((df['Close'].shift(-timegap) >= df['Close'])*1).tolist()[:-timegap] + timegap*[np.nan])
@@ -23,7 +25,25 @@ def create_rate(df, timegap):
 def transform(ma,mi,x):
     return -321*x/(ma-mi) + (45*mi - 366*ma)/(mi-ma)
 
-def generate_image(data, shape = 20):
+def plot_ma_mi_scaler(data_temp, shape = 20):
+    range = data_temp[['Open','High','Low','Close']].max().max() - data_temp[['Open','High','Low','Close']].min().min()
+    if range >3000:
+        scaler = 700
+    elif range > 2000:
+        scaler = 500
+    elif range > 500:
+        scaler = 50
+    elif range > 100:
+        scaler = 10
+    elif range > 20:
+        scaler = 5
+    elif range > 5:
+        scaler = 0.3
+    else:
+        scaler = 0.1
+    return scaler
+
+def generate_image(data, shape = 20, name = ""):
     data_temp = data.copy().tail(shape).reset_index()
 
     my_dpi = 144
@@ -32,7 +52,8 @@ def generate_image(data, shape = 20):
     fig.subplots_adjust(bottom=0.1,left=0.1,right=.9,top=.9)
     candlestick2_ochl(ax, data_temp['Open'], data_temp['Close'], data_temp['High'], data_temp['Low'], 
                     width = 0.5, colordown='r', colorup = 'g')
-    plt.ylim([data_temp[['Open','High','Low','Close']].min().min() - 30, data_temp[['Open','High','Low','Close']].max().max() + 30])
+    plt.ylim([data_temp[['Open','High','Low','Close']].min().min() - plot_ma_mi_scaler(data_temp), 
+              data_temp[['Open','High','Low','Close']].max().max() + plot_ma_mi_scaler(data_temp)])
     ax.yaxis.tick_right()
     
     # awas ini kalau ganti shape
@@ -42,11 +63,11 @@ def generate_image(data, shape = 20):
     plt.close()
     encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
 
-    html ='<img src=\'data:image/png;base64,{}\'>'.format(encoded)
+    html ='<img src=\'data:image/png;base64,{encoded}\'>'.format(encoded = encoded, name = name)
 
     return html[:-1] + ' width="800" height="400" >'
 
-def create_pattern_info_dict(data, label_length = 10):
+def create_pattern_info_dict(data, label_length = 10, name = " "):
     data_temp = data.copy()
 
     pattern = data_temp.columns.drop(['Date','Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
@@ -58,37 +79,39 @@ def create_pattern_info_dict(data, label_length = 10):
     pattern_info_dict = {}
     for i in pattern:
         try:
-            pattern_info_dict[i] = [data_temp.groupby(i).count()['Date'][True]
+            pattern_info_dict[i+ " " + name] = [data_temp.groupby(i).count()['Date'][True]
                                     ,np.round(data_temp.groupby(i).mean()[['label_{}'.format(j) for j in range(1,label_length + 1)]].T[True].values*100,2).tolist()]
         except:
             continue
 
     return pattern_info_dict
 
-def html_interactive_candlestick(data, top, left, shape = 20):
+def html_interactive_candlestick(data, top, left, shape = 20, name = ""):
     temp = data.copy().tail(shape).reset_index()
-    mi ,ma = temp[['Open','High','Low','Close']].min().min() - 30, temp[['Open','High','Low','Close']].max().max() + 30
+    mi ,ma = temp[['Open','High','Low','Close']].min().min() - plot_ma_mi_scaler(temp), temp[['Open','High','Low','Close']].max().max() + plot_ma_mi_scaler(temp)
     html_style_str = ''
     html_body_str = ''
     for i in range(shape):
         html_style_str += '''
-.area-{label}{{
+.area-{name}-{label}{{
     height: {height};
     width: {width};
     top: {top};
     left: {left};
-    border: {border}
+    border: {border};
+    position: absolute;
 }}
         '''.format(label = str(i+1),
            height = str(transform(ma,mi,temp.loc[i,'Low']) - transform(ma,mi,temp.loc[i,'High'])) + 'px',
            width = '13px',
            top = str(transform(ma,mi,temp.loc[i,'High']) + top) + 'px',
            left = str(107 + i*29.15 + left) + 'px',
-           border = '0px solid #555')
+           border = '0px solid #555',
+           name = name)
         
         html_body_str += '''
-<div class="invisible-area area-{label}">
-    <div class="tooltip"><pre>
+<div class="invisible-area area-{name}-{label}">
+    <div class="tooltip"><pre style="z-index:10;color:black;">
 Date :{Date}
 Open :{Open}
 High :{High}
@@ -99,15 +122,16 @@ Vol  :{Vol}</pre>
 </div>
         '''.format(label = str(i+1),
                    Date = str(temp.loc[i,'Date'])[:10],
-                   Open = temp.loc[i,'Open'],
-                   High = temp.loc[i,'High'],
-                   Low = temp.loc[i,'Low'],
-                   Close = temp.loc[i,'Close'],
-                   Vol = temp.loc[i,'Volume'])
+                   Open = np.round(temp.loc[i,'Open'],2),
+                   High = np.round(temp.loc[i,'High'],2),
+                   Low = np.round(temp.loc[i,'Low'],2),
+                   Close = np.round(temp.loc[i,'Close'],2),
+                   Vol = temp.loc[i,'Volume'],
+                   name = name)
 
     return html_style_str, html_body_str
 
-def create_pattern_det_dict(data, shape = 20):
+def create_pattern_det_dict(data, shape = 20, name = ""):
     data_temp = data.copy().tail(shape).reset_index()
 
     horizontal_sum = data_temp[data_temp.columns.drop(['index', 'Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])].sum(axis = 1)
@@ -121,7 +145,7 @@ def create_pattern_det_dict(data, shape = 20):
     
     return pattern_det_dict
 
-def pattern_detection(data,pat_dict, top, left, shape = 20):
+def pattern_detection(data,pat_dict, top, left, shape = 20, name = ""):
     
     with open(local_path + "pattern_img.json", 'r') as f:
         img_dict = json.load(f)
@@ -131,64 +155,159 @@ def pattern_detection(data,pat_dict, top, left, shape = 20):
     pattern_str_html_style = ''
     pattern_str_html_body = ''
     temp = data.copy().tail(shape).reset_index()
-    mi ,ma = temp[['Open','High','Low','Close']].min().min() - 30, temp[['Open','High','Low','Close']].max().max() + 30
+    mi ,ma = temp[['Open','High','Low','Close']].min().min() - plot_ma_mi_scaler(temp), temp[['Open','High','Low','Close']].max().max() + plot_ma_mi_scaler(temp)
     for i in pat_dict:
         pattern_str_html_style += """
-.area-pattern-{label}{{
+.area-{name}-pattern-{label}{{
     height: {height};
     width: {width};
     top: {top};
     left: {left};
-    border: {border}
+    border: {border};
+    position: absolute;
 }}
         """ .format(label = str(i+1),
            height = '30' + 'px',
            width = '13px',
            top = str(transform(ma,mi,temp.loc[i,'High']) - 30 + top) + 'px',
            left = str(107 + i*29.15 + left) + 'px',
-           border = '0px solid #555')
+           border = '0px solid #555',
+           name = name)
         
         image_text = ''
         for pat in pat_dict[i]:
+            if pat != pat_dict[i][0]:
+                image_text += "<hr style=\"color:black;\">"
+
             image_text +="""
-        <pr>
+        <pr style="text-align:center;">
         <p style="text-align:center;width:150px;">
-        {pattern}<br>
+        <b>{pattern}</b><br>
         {image}<br>
         {types}<br>
-        <p class="{pattern_id}" style="text-align:center">{text}</p>
+        <div class="{pattern_id}" style="text-align:center;">{text}</div>
         </p>
         </pr>
-            """.format(image = img_dict[pat][:-1] + ' style= "border:1px solid #555">',
+            """.format(image = img_dict[pat][:-1] + ' style= "border:1px solid #555;margin-left:auto; margin-right:auto;">',
                        types = type_dict[pat].replace('BU', 'Bullish').replace('BE', 'Bearish').replace('CO', 'Continuation').replace('RE', 'Reversal').replace('I', 'Inconclusive'),
                    pattern = pat.replace("_", " "),
-                   pattern_id = pat,
+                   pattern_id = pat + " " + name,
                    text = ''
                    )
 
         pattern_str_html_body += """
-<div class = "invisible-area area-pattern-{label}" ><p class = "pattern"> ! </p>
+<div class = "invisible-area area-{name}-pattern-{label}" ><p class = "pattern" style="text-align:center;" align="center"> ! </p>
     <div class="tooltip">
         {image_text}
     </div>
 </div>
         """.format(label = str(i+1),
-                   image_text = image_text)
+                   image_text = image_text,
+                   name = name)
     return pattern_str_html_style, pattern_str_html_body
 
+def create_stacked(real_html, body, pattern_body, style, pattern_style):
+    full_style = ""
+    full_overview_body = ""
+
+    for i in real_html:
+        full_style += style[i] + pattern_style[i] 
+        full_overview_body += """
+<div id = "{name}">
+    {a}  
+    {b}  
+    {c} 
+</div>
+""".format(name = i,
+           a = real_html[i],
+           b = body[i],
+           c = pattern_body[i])
+
+    return full_style, full_overview_body
+
+def create_option_image(data):
+    option_image = '<select id="mySelect" onchange="changeimage(this.value)" style="position:absolute;;left:150px; top:14px;">'
+    for name in data:
+        option_image += '\n <option value="{name}">{name}</option>'.format(name = name)
+    option_image += '\n </select>'
+    return option_image
+
+def get_recap_table(df):
+    res = pd.DataFrame()
+    for stock in df:
+        temp_df = df[stock].tail(10).copy()
+        temp_df = temp_df[temp_df.columns.drop(['Open','High','Low','Close','Adj Close','Volume'])].set_index('Date')
+        temp_df = temp_df[temp_df==True].stack().reset_index().drop(0, axis = 1)
+        temp_df['Stock'] = stock
+        temp_dict = create_pattern_info_dict(df[stock])
+
+        temp_df['Up Rate'] = [np.round(np.mean(temp_dict[i+'  '][1]),2) for i in temp_df['level_1']]
+        temp_df['Count'] = [temp_dict[i+'  '][0] for i in temp_df['level_1']]
+        temp_df['level_1'] = [i.replace('_', ' ') for i in temp_df['level_1']]
+        res = pd.concat([res,temp_df], axis = 0)
+
+    res = res.sort_values(['Up Rate', 'Date'], ascending = False)
+    res.columns = ['Date', 'Pattern', 'Stock','Up Rate', 'Count']
+    res = res.reset_index().drop('index', axis = 1)
+    res['Date'] = [str(i)[:10] for i in res['Date']]
+    res['Up Rate'] = [str(i) + '%' for i in res['Up Rate']] 
+    return res
+
+def create_table_html(res):
+    html_str_table = '''
+    <table>
+    <tr>
+    '''
+    for col in res.columns:
+        html_str_table += '<th>{col}</th>'.format(col = col)
+    html_str_table += '</tr>'
+
+    for row in range(res.shape[0]):
+        html_str_table += '<tr>'
+        for col in res.columns:
+            html_str_table += '<td>{data}</td>'.format(data = res.loc[row,col])
+        html_str_table += '</tr>'
+    html_str_table += '</table>'
+    return html_str_table
 
 
-def generate_HTML_CS_overview(data_full_pattern, shape = 20, top = 104, left = 13):
+#data_full, shape = 20, top = 104, left = 13):
+def generate_HTML_CS_overview_new(data_full, shape = 20, top = 0, left = 8):
+
     with open(local_path + "pattern_type.json", 'r') as f:
-        type_dict = json.load(f)   
+        type_dict = json.load(f)  
+
+    real_html = {}
+    pattern_info_dict = {}
+    pattern_det_dict = {}
+    style = {}
+    body = {}
+    pattern_style = {} 
+    pattern_body = {}
+    for stock_name1 in data_full:
+        try:
+            stock_name = stock_name1.split('.')[0]
+            iden = '.' + stock_name1.split('.')[1]
+        except:
+            stock_name = stock_name1
+            iden = ''
+
+        stock_name = stock_name.replace(iden, '')
+        df = data_full[stock_name + iden].copy()
+        real_html[stock_name] = generate_image(df, shape = 20, name = stock_name)
+        pattern_info_dict.update(create_pattern_info_dict(df, name = stock_name))
+        pattern_det_dict[stock_name] = create_pattern_det_dict(df)
+        style[stock_name], body[stock_name] = html_interactive_candlestick(data = df ,top = top, left = left, shape = 20, name = stock_name)
+        pattern_style[stock_name], pattern_body[stock_name] = pattern_detection(data = df,pat_dict = pattern_det_dict[stock_name], 
+                                                                                top = top, left = left, name = stock_name)
         
-    df = data_full_pattern.copy()
-    real_html = generate_image(df, shape = 20)
-    pattern_info_dict = create_pattern_info_dict(df)
-    pattern_det_dict = create_pattern_det_dict(df)
-    style, body = html_interactive_candlestick(data = df ,top = top, left = left, shape = 20)
-    pattern_style, pattern_body = pattern_detection(data = df,pat_dict = pattern_det_dict, top = top, left = left)
-    
+    stock_name_list = [i.split('.')[0] for i in list(data_full.keys())]
+    option_image = create_option_image(data = stock_name_list)
+    full_style, full_overview_body = create_stacked(real_html, body, pattern_body, style, pattern_style)
+
+    res = get_recap_table(data_full)
+
+
     return '''
 <!DOCTYPE html>
 <html>
@@ -230,6 +349,9 @@ def generate_HTML_CS_overview(data_full_pattern, shape = 20, top = 104, left = 1
             padding: 6px 12px;
             border: 1px solid #ccc;
             border-top: none;
+            overflow:auto;
+            min-height:450;
+            max-height:500px;
             }
         table {
             width: border-collapse: collapse;
@@ -263,7 +385,11 @@ def generate_HTML_CS_overview(data_full_pattern, shape = 20, top = 104, left = 1
         padding: 1px;
         z-index: 1;
         top: 0px;
-        left: 150%;
+        left: 120%;
+        overflow:auto;
+        max-height:200px;
+        width:155px;
+        opacity:100%;
         }
 
         .invisible-area .tooltip::after {
@@ -288,7 +414,7 @@ def generate_HTML_CS_overview(data_full_pattern, shape = 20, top = 104, left = 1
             text-align:center;
         }
 
-        ''' + style + pattern_style +'''
+        ''' + full_style +'''
 
         </style>
         
@@ -299,45 +425,38 @@ def generate_HTML_CS_overview(data_full_pattern, shape = 20, top = 104, left = 1
 
 <div class="tab">
     <button class="tablinks" onclick="openreport(event, 'Overview')">Overview</button>
-    <button class="tablinks" onclick="openreport(event, 'Top Gain')">Top Gain</button>
-    <button class="tablinks" onclick="openreport(event, 'Top Loss')">Top Loss</button>
+    <button class="tablinks" onclick="openreport(event, 'Top Gain')">Happening </button>
 </div>
 
-<div id="Overview" class="tabcontent">
-    ''' + real_html + body + pattern_body + '''
-<input type="range" name="rangeInput" style ="position:absolute;left:93px; top:124px;" min="1" max="10" value = "5" onchange="updateTextInput(this.value);">
-<input type="text" id="textInput" value="5" size="1" style ="position:absolute;left:233px; top:124px;" >
+<div id="Overview" class="tabcontent" style="position:relative">
+
+''' + option_image +'''
+
+''' + full_overview_body +'''
+
+<p style ="position:absolute;left:105px; top:8px;" min="1">Stock</p>
+<p style ="position:absolute;left:480px; top:10px;" min="1">Time-Gap</p>
+<input type="range" name="rangeInput" style ="position:absolute;left:550px; top:14px;width:135px;" min="1" max="10" value = "5" onchange="updateTextInput(this.value);">
+<input type="text" id="textInput" value="5" size="1" style ="position:absolute;left:690px; top:14px;" >
+
+
 
 </div>
 <div id="Top Gain" class="tabcontent">
-    <h3>Top Gain</h3>
-    <table>
-        <tr>
-            <th>Pattern</th>
-            <th>Accuracy</th>
-            <th>Rate</th>
-        </tr>
-        </tr>
-    </table>
+    <h3>Happening Pattern</h3>
+''' + create_table_html(res) +'''
 </div>
 
-<div id="Top Loss" class="tabcontent">
-    <h3>Top Loss</h3>
-    <table>
-        <tr>
-            <th>Pattern</th>
-            <th>Accuracy</th>
-            <th>Rate</th>
-        </tr>
-    </table>
-</div>
+
+<p id = "demo"><p>
 
 <script>
 
-
 var acc_dicts = ''' + str(pattern_info_dict) + ''';
 var type_dicts = ''' + str(type_dict) + ''';
+var stock_name = ''' + str(stock_name_list) + ''';
 
+changeimage(stock_name[0])
 updateTextInput(5)
 
 function updateTextInput(val) {
@@ -346,13 +465,13 @@ function updateTextInput(val) {
         var x = document.getElementsByClassName(key);
         var i;
         for (i = 0; i < x.length; i++) {
-            if ((type_dicts[key] == "BU RE")||(type_dicts[key] == "BU CO")||(type_dicts[key] == "I")){
-                x[i].innerHTML = "<p style='color:green;font-size:20px;'>&#10138;<span style='color:black;font-size:15px;'>" 
+            if ((type_dicts[key.split(" ")[0]] == "BU RE")||(type_dicts[key.split(" ")[0]] == "BU CO")||(type_dicts[key.split(" ")[0]] == "I")){
+                x[i].innerHTML = "<p style='color:green;font-size:20px;text-align:center;'>&#10138;<span style='color:black;font-size:15px;'>" 
                 + acc_dicts[key][1][val-1] + "%</span></p>" +  (acc_dicts[key][0]*acc_dicts[key][1][val-1]/100).toFixed(0) + 
                 " <span style='color:green;font-weight:10px'>Up</span> out of " + acc_dicts[key][0];
                 }
             else{
-                x[i].innerHTML = "<p style='color:red;font-size:20px;'>&#10136;<span style='color:black;font-size:15px;'>" 
+                x[i].innerHTML = "<p style='color:red;font-size:20px;text-align:center;'>&#10136;<span style='color:black;font-size:15px;'>" 
                 + (100 - acc_dicts[key][1][val-1]).toFixed(2) + "%</span></p>" +(acc_dicts[key][0]*(100-acc_dicts[key][1][val-1])/100).toFixed(0) + 
                 " <span style='color:red;font-weight:10px'>Down</span> out of " + acc_dicts[key][0];
             }
@@ -372,6 +491,13 @@ function openreport(evt, cityName) {
   }
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+function changeimage(val){
+    for (i = 0; i < stock_name.length; i++){
+        document.getElementById(stock_name[i]).style.display = "none";
+    }
+    document.getElementById(val).style.display = "block";
 }
 </script>
    
--- Stockie/stockie.py
+++ Stockie/stockie.py
@@ -1,96 +1,96 @@
+# from Stockie.stockie import stockie
 from .Candlestick import is_pattern
 from .report import *
+# from Stockie.stockie import stockie
 import yfinance as yf
-from IPython.core.display import display, HTML
+from IPython.core.display import display, HTML, clear_output
 import pandas as pd
 from mpl_finance import candlestick2_ochl
 import matplotlib.pyplot as plt
-import json
+import seaborn as sns
 import numpy as np
+import json
 import os
 
 import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
 
+
 class stockie:
     def __init__(self, stock_name):
         self.stock_name = stock_name
-        self.data = yf.download(stock_name).reset_index()
-        data = self.data
-        for i in ['Open','High','Low','Close']:
-            if i not in data.columns:
-                break
-                raise exception('{} is not in table\'s columns'.format(i))
-            else:
-                continue
-        self.open = data['Open'].values
-        self.high = data['High'].values
-        self.low = data['Low'].values
-        self.close = data['Close'].values   
+        self.data = {}
 
-        self.open_h1 = data['Open'].shift(1).values
-        self.high_h1 = data['High'].shift(1).values
-        self.low_h1 = data['Low'].shift(1).values
-        self.close_h1 = data['Close'].shift(1).values  
-
-        self.open_h2 = data['Open'].shift(2).values
-        self.high_h2 = data['High'].shift(2).values
-        self.low_h2 = data['Low'].shift(2).values
-        self.close_h2 = data['Close'].shift(2).values  
+        for stock in self.stock_name:
+            self.data[stock] = yf.download(stock).reset_index()
+        clear_output()
 
         print('{} data loaded successfully'.format(stock_name))
-        print(os.getcwd())
 
-    def find_pattern(self, name ='all'):
-        
-        single_pattern = ['doji', 'gravestone_doji', 'dragonfly_doji', 'bullish_spinning_top', 'bearish_spinning_top',
+        self.additional_pattern = {}
+
+        self.single_pattern = ['doji', 'gravestone_doji', 'dragonfly_doji', 'bullish_spinning_top', 'bearish_spinning_top',
                           'inverted_Hammer', 'shootingstar', 'hanging_man', 'hammer', 'bullish_marubozu', 'bearish_marubozu',
                           'bullish_belt_hold', 'bearish_belt_hold']
-        double_pattern = ['bullish_engulfing', 'bearish_engulfing', 'tweezer_top', 'tweezer_bottom','bullish_separating_line',
+        self.double_pattern = ['bullish_engulfing', 'bearish_engulfing', 'tweezer_top', 'tweezer_bottom','bullish_separating_line',
                           'bullish_separating_line', 'piercing_line', 'dark_cloud_cover', 'matching_high', 'matching_low',
                           'bullish_harami', 'bearish_harami', 'bullish_harami_cross', 'bearish_harami_cross', 'descending_hawk',
                           'homing_pigeon', 'bullish_in_neck', 'bearish_in_neck', 'bullish_on_neck','bearish_on_neck','bullish_kicking',
                           'bearish_kicking']
-        triple_pattern = ['morning_star', 'evening_star', 'morning_doji_star', 'evening_doji_star', 'three_white_soldier', 'three_black_crow',
+        self.triple_pattern = ['morning_star', 'evening_star', 'morning_doji_star', 'evening_doji_star', 'three_white_soldier', 'three_black_crow',
                           'three_inside_up', 'three_inside_down', 'deliberation', 'three_outside_up', 'three_outside_down', 'bullish_abandoned_baby',
                           'bearish_abandoned_baby', 'bullish_stick_sandwich', 'bearish_stick_sandwich', 'bullish_side_by_side_white_line', 
                           'bearish_side_by_side_black_line', 'bearish_side_by_side_white_line', 'bullish_side_by_side_black_line',
                           'upside_gap_three', 'downside_gap_three', 'upside_gap_two_crow', 'unique_three_river_bottom', 'bullish_tri_star', 
                           'bearish_tri_star', 'three_star_in_the_north', 'three_star_in_the_south']
 
+        self.pattern = self.single_pattern + self.double_pattern + self.triple_pattern
+
+    def add_pattern(self, name, function, type = 'I'):
+        if (name not in self.pattern)|(name not in self.additional_pattern):
+            self.additional_pattern[name] = function
+        else:
+            print('pattern name has been taken, try another one')
+
+    def find_pattern(self, name ='all'):
+        all_pattern_data = {}
+
+        for data_key in self.data:
+            data = self.data[data_key]
+
+            open = data['Open'].values
+            high = data['High'].values
+            low = data['Low'].values
+            close = data['Close'].values   
+
+            open_h1 = data['Open'].shift(1).values
+            high_h1 = data['High'].shift(1).values
+            low_h1 = data['Low'].shift(1).values
+            close_h1 = data['Close'].shift(1).values  
+
+            open_h2 = data['Open'].shift(2).values
+            high_h2 = data['High'].shift(2).values
+            low_h2 = data['Low'].shift(2).values
+            close_h2 = data['Close'].shift(2).values  
         
-        if name == 'single':
-            temp = self.data.copy()
-            for i in single_pattern:
-                temp[i] = is_pattern(i, [self.open, self.high, self.low, self.close],
-                                     [self.open_h1, self.high_h1, self.low_h1, self.close_h1],
-                                     [self.open_h2, self.high_h2, self.low_h2, self.close_h2])
-                
-        if name == 'double':
-            temp = self.data.copy()
-            for i in double_pattern:
-                temp[i] = is_pattern(i, [self.open, self.high, self.low, self.close],
-                                     [self.open_h1, self.high_h1, self.low_h1, self.close_h1],
-                                     [self.open_h2, self.high_h2, self.low_h2, self.close_h2])
-            return temp
 
-        if name == 'triple':
-            temp = self.data.copy()
-            for i in triple_pattern:
-                temp[i] = is_pattern(i, [self.open, self.high, self.low, self.close],
-                                     [self.open_h1, self.high_h1, self.low_h1, self.close_h1],
-                                     [self.open_h2, self.high_h2, self.low_h2, self.close_h2])
-            return temp
+            if name == 'all':
+                temp = data.copy()
+                for i in self.pattern:
+                    temp[i] = is_pattern(i, [open, high, low, close],
+                                        [open_h1, high_h1, low_h1, close_h1],
+                                        [open_h2, high_h2, low_h2, close_h2])
+                for i in self.additional_pattern:
+                    temp[i] = self.additional_pattern[i]()
 
-        if name == 'all':
-            temp = self.data.copy()
-            for i in single_pattern + double_pattern + triple_pattern:
-                temp[i] = is_pattern(i, [self.open, self.high, self.low, self.close],
-                                     [self.open_h1, self.high_h1, self.low_h1, self.close_h1],
-                                     [self.open_h2, self.high_h2, self.low_h2, self.close_h2])
-            return temp
+            all_pattern_data[data_key] = temp
+        return all_pattern_data
 
-    def get_candlestick_report(self,shape=20):
-
-        return display(HTML(generate_HTML_CS_overview(self.find_pattern(), shape= 20)))
+    def get_candlestick_report(self,shape=20, create_HTML_file = False, location = ''):
+        clear_output()
+        html_str = generate_HTML_CS_overview_new(self.find_pattern(), shape= 20)
+        if create_HTML_file:
+            with open(location, 'w') as f:
+                f.write(html_str)
+        return display(HTML(html_str))
